e110d2e0227ff07a566306a20750efbe0a5fa33a
Hegar Garcia <hegargarcia@gmail.com>
2020-07-29 11:20:45 -0500

    Moving back to javascript Removing typescript due to problems with change detection

--- src/main/bootstrap.jsx
+++ src/main/bootstrap.jsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import React from "react";
 import { render } from "react-dom";
 import Root from "./Root";
-import { register } from "src/main/offline";
+import { register } from "./offline/offline";
 
 render(<Root />, document.getElementById("app"));
 register();
